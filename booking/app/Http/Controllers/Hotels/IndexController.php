d5193786128f85150e20341691a2c5ea7e7e0bfe
R-Artyom <ryabov.artyom@gmail.com>
2023-09-30 20:13:33 +0700

    Страница списка отелей - добавлен расчет цены (диапазона цен) за 1 ночь для каждого отеля ($hotel->price), теперь отображается

--- booking/app/Http/Controllers/Hotels/IndexController.php
+++ booking/app/Http/Controllers/Hotels/IndexController.php
@@ -11,10 +11,28 @@ class IndexController extends Controller
     // Список отелей
     public function __invoke()
     {
-        // Отели
-        $hotels = Hotel::all();
+        // Отели с удобствами и комнатами
+        $hotels = Hotel::with('facilities')->with('rooms')->get();
+
+        // Диапазон цен за 1 ночь
+        $hotels->map(function ($hotels) {
+            // Минимальная цена за номер
+            $minPrice = $hotels->rooms->min('price');
+            // Максимальная цена за номер
+            $maxPrice = $hotels->rooms->max('price');
+            // Если они равны, то итог - любая одна
+            if ($minPrice === $maxPrice) {
+                $price = $minPrice;
+            // Если цены разные - то диапазон
+            } else {
+                $price = $minPrice . '-' . $maxPrice;
+            }
+            // Новое поле в каждом элементе коллекции
+            $hotels['price'] = $price;
+            return $hotels;
+        });
+
         // Шаблон отелей
         return view('hotels.index', compact('hotels'));
     }
-
 }
