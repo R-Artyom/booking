f51979cbf5d07bb9d6f554ac53e50d6f7e2cc951
R-Artyom <ryabov.artyom@gmail.com>
2023-11-22 18:05:18 +0700

    При попытке бронирования номера незарегистрированным пользователем происходит перенаправление на страницу авторизации

--- booking/resources/views/components/rooms/room-list-item.blade.php
+++ booking/resources/views/components/rooms/room-list-item.blade.php
@@ -23,13 +23,19 @@
                 <span class="text-lg font-bold">{{ $room->total_price }} руб.</span>
                 <span>за {{ $room->total_days }} ночей</span>
             </div>
-            <form class="ml-4" method="POST" action="{{ route('bookings.store') }}">
-                @csrf
-                <input type="hidden" name="started_at" min="{{ date('Y-m-d') }}" value="{{ request()->get('start_date', \Carbon\Carbon::now()->format('Y-m-d')) }}">
-                <input type="hidden" name="finished_at" min="{{ date('Y-m-d') }}" value="{{ request()->get('end_date', \Carbon\Carbon::now()->addDay()->format('Y-m-d')) }}">
-                <input type="hidden" name="room_id" value="{{ $room->id }}">
-                <x-the-button class=" h-full w-full">{{ __('Book') }}</x-the-button>
-            </form>
+            @if(auth()->check())
+                <form class="ml-4" method="POST" action="{{ route('bookings.store') }}">
+                    @csrf
+                    <input type="hidden" name="started_at" min="{{ date('Y-m-d') }}" value="{{ request()->get('start_date', \Carbon\Carbon::now()->format('Y-m-d')) }}">
+                    <input type="hidden" name="finished_at" min="{{ date('Y-m-d') }}" value="{{ request()->get('end_date', \Carbon\Carbon::now()->addDay()->format('Y-m-d')) }}">
+                    <input type="hidden" name="room_id" value="{{ $room->id }}">
+                    <x-the-button class=" h-full w-full">{{ __('Book') }}</x-the-button>
+                </form>
+            @else
+                <div class="ml-4 flex">
+                    <x-link-button href="{{ route('login') }}">{{ __('Book') }}</x-link-button>
+                </div>
+            @endif
         </div>
     </div>
 </div>
