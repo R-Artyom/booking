34d235adaccb858c067f56d202de99d6d907e105
R-Artyom <ryabov.artyom@gmail.com>
2023-11-29 13:59:05 +0700

    Реализация пагинации для списка отелей

--- booking/app/Http/Controllers/Hotels/IndexController.php
+++ booking/app/Http/Controllers/Hotels/IndexController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Hotels;
 
 use App\Http\Controllers\Controller;
 use App\Models\Hotel;
-use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
@@ -12,9 +11,9 @@ class IndexController extends Controller
     public function __invoke()
     {
         // Отели с удобствами и комнатами
-        $hotels = Hotel::with(['facilities', 'rooms'])->get();
+        $hotels = Hotel::with(['facilities', 'rooms'])->paginate(6);
         // Диапазон цен за 1 ночь
-        $hotels->map(function ($hotels) {
+        $hotels->map(function($hotels) {
             // Минимальная цена за номер
             $minPrice = $hotels->rooms->min('price');
             // Максимальная цена за номер
--- booking/resources/views/hotels/index.blade.php
+++ booking/resources/views/hotels/index.blade.php
@@ -1,9 +1,10 @@
 <x-app-layout>
     <div class="py-14 px-4 md:px-6 2xl:px-20 2xl:container 2xl:mx-auto">
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
             @foreach($hotels as $hotel)
                 <x-hotels.hotel-card :hotel="$hotel"></x-hotels.hotel-card>
             @endforeach
         </div>
+        {{ $hotels->onEachSide(1)->withQueryString()->links() }}
     </div>
 </x-app-layout>
