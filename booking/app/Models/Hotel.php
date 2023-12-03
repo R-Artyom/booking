f3b58f4e0e301a802e973f3be631dd9d46f34e67
R-Artyom <ryabov.artyom@gmail.com>
2023-12-03 18:59:19 +0700

    Для атрибута poster_url модели Hotel создан аксессор, чтобы при его запросе ссылка всегда была рабочая и начиналась с http, т.к. faker делает ссылку на "https://via.placeholder.com...", а при локальном создании отеля и редактировании ссылка будет начинаться с "/hotels/.."

--- booking/app/Models/Hotel.php
+++ booking/app/Models/Hotel.php
@@ -26,4 +26,22 @@ class Hotel extends Model
         // Связь с таблицей rooms - один ко многим
         return $this->hasMany(Room::class, 'hotel_id', 'id');
     }
+
+    /**
+     * Аксессор - Преобразование атрибута poster_url (URL изображения отеля) при запросе
+     *
+     * @param string $value Оригинальное значение атрибута poster_url
+     * @return string - Преобразованное значение атрибута poster_url
+     */
+    public function getPosterUrlAttribute(string $value): string
+    {
+        // Если ссылка готовая (изображение хранится в сетке) - то изменять не надо
+        if (substr($value, 0, 4) === 'http') {
+            return $value;
+        // Если ссылка не начинается на http (изображение хранится локально в папке public) - то надо сформировать ссылку
+        } else {
+            // Ссылка вида http://.../storage/hotels/JcxrrCpweNqU1LRv7UmNeNGEk4ejDtGgsosNJNRD.jpg
+            return asset('storage/' . $value);
+        }
+    }
 }
