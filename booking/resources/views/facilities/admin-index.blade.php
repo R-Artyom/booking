9ed78bfa372869e2f1620810a1c7ac86a72e2661
R-Artyom <ryabov.artyom@gmail.com>
2024-01-07 20:53:43 +0700

    Правка таблицы удобств

--- booking/resources/views/facilities/admin-index.blade.php
+++ booking/resources/views/facilities/admin-index.blade.php
@@ -10,20 +10,20 @@
             <table class="min-w-full bg-white">
                 <thead class="bg-gray-800 text-white">
                     <tr>
-                        <th class="w-1/4 text-left py-2 px-4 uppercase font-semibold text-sm">Номер</th>
-                        <th class="w-1/4 text-left py-2 px-4 uppercase font-semibold text-sm">Название</th>
-                        <th class="w-1/4 text-left py-2 px-4 uppercase font-semibold text-sm">Дата создания</th>
-                        <th class="w-1/4 text-left py-2 px-4 uppercase font-semibold text-sm"></th>
+                        <th class="text-left py-2 px-4 uppercase font-semibold text-sm">№</th>
+                        <th class="text-left py-2 px-4 uppercase font-semibold text-sm">Название</th>
+                        <th class="text-left py-2 px-4 uppercase font-semibold text-sm">Дата создания</th>
+                        <th class="text-left py-2 px-4 uppercase font-semibold text-sm"></th>
                     </tr>
                 </thead>
                 <tbody class="text-gray-700">
                     @for($i = 0; $i < count($facilities); $i++)
                         {{-- Четная строка - белая, н/ч - серая --}}
                         <tr @if($i % 2 !== 0) class="bg-gray-200" @endif >
-                            <td class="w-1/4 text-left py-1 px-4">{{ $facilities[$i]->id }}</td>
-                            <td class="w-1/4 text-left py-1 px-4">{{ $facilities[$i]->name }}</td>
-                            <td class="w-1/4 text-left py-1 px-4">{{ $facilities[$i]->created_at }}</td>
-                            <td class="w-1/4 text-left py-1 px-4">
+                            <td class="text-left py-1 px-4">{{ $facilities[$i]->id }}</td>
+                            <td class="text-left py-1 px-4">{{ $facilities[$i]->name }}</td>
+                            <td class="text-left py-1 px-4">{{ $facilities[$i]->created_at }}</td>
+                            <td class="text-left py-1 px-4">
                                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('admin.facilities.edit', ['facility' => $facilities[$i]]) }}">
                                     &#9998; Редактировать
                                 </a>
