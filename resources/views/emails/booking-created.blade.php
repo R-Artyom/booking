7939ef77edecdf242ceed1d454a67de70168738d
R-Artyom <ryabov.artyom@gmail.com>
2024-02-05 02:14:52 +0700

    Корректировка текста уведомлений о создании и отмене бронирований

--- resources/views/emails/booking-created.blade.php
+++ resources/views/emails/booking-created.blade.php
@@ -1,17 +1,24 @@
 {{--Тело письма "Создание бронирования"--}}
-Служба размещения отеля <a href="{{ route('hotels.show', ['hotel' => $booking->room->hotel]) }}" target="_blank" title="Откроется в новой вкладке">"{{ $booking->room->hotel->name }}"</a> подтверждает бронирование согласно Вашей заявке:<br>
+<h2>Здравствуйте, {{ $booking->user->name }}!</h2>
+Служба бронирования отеля <a href="{{ route('hotels.show', ['hotel' => $booking->room->hotel]) }}" target="_blank" title="Откроется в новой вкладке">"{{ $booking->room->hotel->name }}"</a> подтверждает бронирование согласно Вашей заявке.<br>
+<hr>
+<h2>Детали бронирования</h2>
+<u><b>Бронирование №{{ $booking->id }}</b></u><br>
 <b>ФИО гостя:</b> {{ $booking->user->name }}<br>
 <b>Период проживания:</b> {{ date('d.m.Y', strtotime($booking->started_at)) }} - {{ date('d.m.Y', strtotime($booking->finished_at)) }}<br>
 <b>Номер:</b> {{ $booking->room->name }}<br>
 <b>Площадь номера:</b> {{ $booking->room->floor_area }} м2<br>
 <b>Описание номера:</b> {{ $booking->room->description }}<br>
 <b>Тип номера:</b> {{ $booking->room->type }} <br>
-<b>Стоимость номера:</b> {{ $booking->room->price }} руб. за ночь<br>
+<b>Стоимость номера за ночь:</b> {{ $booking->room->price }} руб.<br>
 <b>Общая стоимость номера за весь период проживания:</b> {{ $booking->price }} руб.<br>
 <b>В стоимость номера входит:</b> {{ implode(', ',$booking->room->facilities->pluck('name')->toArray()) }}<br>
 <br>
-"Благодарим за выбор нашего отеля!<br>
-<br>
-"Отель "{{ $booking->room->hotel->name }}"<br>
 {{--Ссылка на карточку бронирования--}}
-"<a href="{{ route('bookings.show', ['booking' => $booking]) }}" target="_blank" title="Откроется в новой вкладке">Посмотреть карточку бронирования</a> <br>
+<a href="{{ route('bookings.show', ['booking' => $booking]) }}" target="_blank" title="Откроется в новой вкладке">Посмотреть карточку бронирования</a><br>
+<hr>
+<br>
+Благодарим за выбор нашего отеля!<br>
+<br>
+С уважением,<br>
+служба бронирования <a href="{{ route('hotels.show', ['hotel' => $booking->room->hotel]) }}" target="_blank" title="Откроется в новой вкладке">"{{ $booking->room->hotel->name }}"</a>.
