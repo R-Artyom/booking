a5fb49b6f21b072ceb7179a6fff4662a68b9c0f1
R-Artyom <ryabov.artyom@gmail.com>
2024-03-15 11:07:57 +0700

    Рефакторинг карточки бронирования (размер шрифта, интервалы, ссылка на отель)

--- resources/views/bookings/show.blade.php
+++ resources/views/bookings/show.blade.php
@@ -4,9 +4,11 @@
     <!-- component -->
     <div class="py-14 px-4 md:px-6 2xl:px-20 2xl:container 2xl:mx-auto">
         <div class="flex justify-start item-start space-y-2 flex-col">
-            <h1 class="text-3xl lg:text-4xl font-semibold leading-7 lg:leading-9 text-gray-800">Отель {{ $booking->room->hotel->name }}</h1>
+            <a class="text-2xl font-bold text-gray-800" href="{{ route('hotels.show', ['hotel' => $booking->room->hotel]) }}">
+                Отель {{ $booking->room->hotel->name }}
+            </a>
         </div>
-        <div class="mt-10 flex flex-col xl:flex-row jusitfy-center items-stretch w-full xl:space-x-8 space-y-4 md:space-y-6 xl:space-y-0">
+        <div class="mt-6 flex flex-col xl:flex-row jusitfy-center items-stretch w-full xl:space-x-8 space-y-4 md:space-y-6 xl:space-y-0">
             <x-users.user-card :user="$booking->user"></x-users.user-card>
             <x-bookings.booking-card :booking="$booking"></x-bookings.booking-card>
         </div>
